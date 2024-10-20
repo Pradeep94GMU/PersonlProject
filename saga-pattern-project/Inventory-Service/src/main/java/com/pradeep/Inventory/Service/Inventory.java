8008afa0293b50788bb771730ecc9ea5e9753ca9
Pradeep Ranjan <pradeepranjan@Pradeeps-MacBook-Pro.local>
2024-10-20 16:41:28 -0400

    testing

---
+++ saga-pattern-project/Inventory-Service/src/main/java/com/pradeep/Inventory/Service/Inventory.java
@@ -0,0 +1,25 @@
+package com.pradeep.Inventory.Service;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private int stock;
+
+    // Getters and Setters
+}
